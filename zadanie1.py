c9f41eb9de146e61d71613db267d4fd34a1b4e1e
dvdjnt <dvdjnt@protonmail>
2023-03-24 07:49:46 +0100

    working graphs

--- zadanie1.py
+++ zadanie1.py
@@ -1,7 +1,14 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+def getSizeOf(object):
+    counter = 0
+    for item in object:
+        counter+=1
+    return counter
+
 def translate(mode,input_matrix,d):
+    print('translate '+mode+', d:'+str(d))
     Tx = np.array([
         [1, 0, 0, d],
         [0, 1, 0, 0],
@@ -21,11 +28,11 @@ def translate(mode,input_matrix,d):
         [0, 0, 0, 1]])
     
     if mode == "x":
-        return Tx.dot(input)
+        return Tx.dot(input_matrix)
     if mode == "y":
-        return Ty.dot(input)
+        return Ty.dot(input_matrix)
     if mode == "z":
-        return Tz.dot(input)
+        return Tz.dot(input_matrix)
      
 def rotate(mode,input_matrix,angle):
     Rx = np.array([
@@ -47,11 +54,11 @@ def rotate(mode,input_matrix,angle):
         [0, 0, 0, 1]])
     
     if mode == "x":
-        return Rx.dot(input)
+        return Rx.dot(input_matrix)
     if mode == "y":
-        return Ry.dot(input)
+        return Ry.dot(input_matrix)
     if mode == "z":
-        return Rz.dot(input)   
+        return Rz.dot(input_matrix)
 
 # [mm]
 l1 = 203
@@ -60,10 +67,18 @@ l3 = 178
 
 fi1_min = -90
 fi1_max = 90
+
 fi2_min = -55
 fi2_max = 125
+
 fi3_min = 0
 fi3_max = 150
+
+# ----------------
+
+fi1 = 80
+fi2 = 100
+fi3 = 90
 
 # -------------
 matrix_one = np.array([
@@ -79,37 +94,56 @@ vector_one = np.array([
     ])
 
 # --------------
-B = np.array([
+origin = np.array([
     [0],
     [0],
     [0],
     [1]
     ])
 
-#A = B.dot(vector_one) # nasobenie matic
-#print(A)
 
-
-A = translate("x",B,5)
-print(type(A))
-"""
-A = translate("y",A,8)
-print(type(A))
-
-A = translate("z",A,-2)
-
+A = translate("z", origin, l1)
+print('A')
 print(A)
 
-xx = np.vstack([x[0::2],x[1::2]])
-yy = np.vstack([y[0::2],y[1::2]])
+B = rotate("x", A, fi2)
+B = translate("z", B, l2)
+print('B')
+print(B)
+
+C = rotate("x", B, fi3)
+C = translate("z", C, l3)
+print('C')
+print(C)
+
+#xx = np.vstack([x[0::2], x[1::2]])
+#yy = np.vstack([y[0::2], y[1::2]])
 
 fig = plt.figure()
-for i in range(0, len(A), 2):
-    plt.plot(A[i:i+2], A[i:i+2], 'ro-')
-
-
-
 ax = plt.axes(projection="3d")
-ax.plot3D(A[0],A[1],A[2],'red')
+
+points = [origin, A, B, C]
+
+sizeOfPoints = getSizeOf(points)
+
+print('points indexing..')
+print(points[0][0])
+print(points[0][1])
+print(points[0][2])
+
+
+print('cycle')
+
+for i in range(0, sizeOfPoints-1):
+    # plot([A(x),B(x)],[A(y),B(y)],[A(z),B(z)])
+    # for j in range(0,3):
+    plt.plot([points[i][0], points[i+1][0]], [points[i][1], points[i+1][1]], [points[i][2], points[i+1][2]])
+
+
+# for i in range(0, len(A), 2):
+#     plt.plot(A[i:i+2], A[i:i+2], 'ro-')
+#
+#
+ax.plot3D(A[0], A[1], A[2], 'red', label='robotic manipulator')
 plt.show()
-"""
+
