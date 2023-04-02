1a852762e015e008cb10f991f7b684ea1713d943
dvdjnt <dvdjnt@protonmail>
2023-04-02 20:27:23 +0200

    adjusted robotic arm thickness

--- zadanie1.py
+++ zadanie1.py
@@ -283,7 +283,7 @@ for i in range(len(points) - 1):
     xs = [points[i, 0], points[i + 1, 0]]
     ys = [points[i, 1], points[i + 1, 1]]
     zs = [points[i, 2], points[i + 1, 2]]
-    line, = ax.plot(xs, ys, zs, color='orange')
+    line, = ax.plot(xs, ys, zs, color='orange', linewidth=3)
     lines.append(line)
 
 # vektory smeru (posunute suradnicove systemy)
