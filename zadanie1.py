94fb6d50caaa368375347f7a3e132e19883d8b0d
dvdjnt <dvdjnt@protonmail>
2023-04-02 02:15:03 +0200

    working RCVS (relative coordinate vector system)

--- zadanie1.py
+++ zadanie1.py
@@ -171,6 +171,10 @@ def calculatePoints(fi1, fi2, fi3, l1, l2, l3):
 def calculateCoordinateVectors(fi1, fi2, fi3, l1, l2, l3):
     # vectors of coordinate system
     d = 50  # scaling of vector
+    fi1 = deg2rad(fi1)
+    fi2 = deg2rad(fi2)
+    fi3 = deg2rad(fi3)
+
     relativeSystem = np.array([
         [d, 0, 0, 1],
         [0, d, 0, 1],
@@ -202,6 +206,7 @@ def update(val, points):
     print('points more')
     print(points)
     points = calculatePoints(slider_fi1.val, slider_fi2.val, slider_fi3.val, l1, l2, l3)
+    vectors = calculateCoordinateVectors(slider_fi1.val, slider_fi2.val, slider_fi3.val, l1, l2, l3)
     # coordinateVectors = calculateCoordinateVectors()
     # pridat vykreslenie vektorov
     # scatter navyse
@@ -217,8 +222,22 @@ def update(val, points):
         zs = [points[i, 2], points[i + 1, 2]]
         lines[i].set_data(xs, ys)
         lines[i].set_3d_properties(zs)
-    fig.canvas.draw_idle()
+    # fig.canvas.draw_idle()
 
+    colors = ['red', 'green', 'blue']
+    j = 0
+    for i in range(0, len(points)):
+        k = 0
+        while k < 3:
+            xs = [points[i][0], vectors[j][0]]
+            ys = [points[i][1], vectors[j][1]]
+            zs = [points[i][2], vectors[j][2]]
+            linesVectors[j].set_data(xs, ys)
+            linesVectors[j].set_3d_properties(zs)
+            k = k + 1
+            j = j + 1
+        # fig.canvas.draw_idle()
+    fig.canvas.draw()
 
 # -------------
 matrix_one = np.array([
@@ -278,12 +297,6 @@ for i in range(len(points) - 1):
     zs = [points[i, 2], points[i + 1, 2]]
     line, = ax.plot(xs, ys, zs, color='orange')
     lines.append(line)
-    print(i)
-    print(xs)
-    print()
-    print(ys)
-    print()
-    print(zs)
 
 # vektory smeru (posunute suradnicove systemy)
 colors = ['red', 'green', 'blue']
@@ -299,23 +312,8 @@ for i in range(0, len(points)):
         linesVectors.append(line)
         j = j + 1
 
-    # xs = [points[i][0], coordinateVectors[i][0]]
-    # ys = [points[i][1], coordinateVectors[i][1]]
-    # zs = [points[i][2], coordinateVectors[i][2]]
-    # print(i)
-    # print(xs)
-    # print()
-    # print(ys)
-    # print()
-    # print(zs)
-    # #
-    # # xs = [coordinateVectors[i, 0], coordinateVectors[i + 1, 0]]
-    # # ys = [coordinateVectors[i, 1], coordinateVectors[i + 1, 1]]
-    # # zs = [coordinateVectors[i, 2], coordinateVectors[i + 1, 2]]
-    # line, = ax.plot(xs, ys, zs)
-    # lines.append(line)
-
-# quit()
+# dvakrat ten isty kod vykreslenia, len v update je len refresh akoby
+# inak by zostavali veci na obrazovke
 
 # sliders
 sliders = []
@@ -341,6 +339,7 @@ for slider_fi1, slider_fi2, slider_fi3 in sliders:
     slider_fi2.on_changed(lambda val: update(val, points))
     slider_fi3.on_changed(lambda val: update(val, points))
 
+
 # render
 # plot config
 ax.set_xlabel('X')
@@ -350,3 +349,4 @@ ax.set_title('roboticky manipulator 3D', loc='center', fontsize=30)
 ax.axis('equal')
 fig.subplots_adjust(top=0.9, bottom=0.15)
 plt.show()
+print('wait')
